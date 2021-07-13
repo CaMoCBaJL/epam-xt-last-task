daa99258b647698936e2b571bf76ab5505df9dab
DESKTOP-VHEEB1U\gorka <gorka19800@gmail.com>
2021-07-13 13:16:04 +0300

    Added Error page and enabled custom error routing

---
+++ OnlineRecipeBook/WebPL/Models/DataToRoute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPL.Models
+{
+    public static class DataToRoute
+    {
+        public static int RecipeId { get; set; }
+
+        public static int UserId { get; set; }
+    }
+}
