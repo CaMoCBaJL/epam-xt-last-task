42a9aa1ef2b5ae5a64be55502a1137cfd5962f81
DESKTOP-VHEEB1U\gorka <gorka19800@gmail.com>
2021-07-16 21:09:50 +0300

    Added delete confirmation page and EntityToDeleteData to incapsulated avoid problem, connected with repeat of post-querry on page refreshing

---
+++ OnlineRecipeBook/WebPL/Models/EntityToDeleteData.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WebPL.Models
+{
+    public static class EntityToDeleteData
+    {
+        public static string SourcePage { get; set; }
+
+        public static int EntityId { get; set; }
+
+        public static bool ConfirmationResult { get; set; }
+
+        public static string EntityName { get; set; }
+    }
+}
