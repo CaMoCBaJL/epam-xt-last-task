5498b74b63a34d954d0a3e763ba8871e3d0071e8
DESKTOP-VHEEB1U\gorka <gorka19800@gmail.com>
2021-07-19 15:47:57 +0300

    Added opportunity to change password display on login and registration pages

---
+++ OnlineRecipeBook/WebPL/scripts/showPassword.js
@@ -0,0 +1,11 @@
+﻿
+function showPassword(elemId) {
+    var c = elemId;
+
+    if (document.getElementById(elemId).type == 'password') {
+        document.getElementById(elemId).type = 'text'
+    }
+    else {
+        document.getElementById(elemId).type = 'password'
+    }
+}
